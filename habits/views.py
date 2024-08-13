ec3c6aad1a39644f3b5a9fc2a1ce78ce33468cf7
kasyan1337 <tristokorun@gmail.com>
2024-08-13 10:59:40 +0200

    PublicHabitListView

--- habits/views.py
+++ habits/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics, permissions
+
 from habits.models import Habit
 from habits.serializers import HabitSerializer
 from .permissions import IsOwnerOrReadOnly
+
 
 class HabitListCreateView(generics.ListCreateAPIView):
     serializer_class = HabitSerializer
@@ -13,9 +15,19 @@ class HabitListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+
 class HabitRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = HabitSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
     def get_queryset(self):
         return Habit.objects.all()
+
+
+class PublicHabitListView(generics.ListAPIView):
+
+    serializer_class = HabitSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True)
